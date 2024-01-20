15a53489596814afeea3e6ad21154ae9931ee45f
fatah <fatah@sahabatbisnis.co.id>
2024-01-20 13:10:00 +0700

    refactoring

--- connect.py
+++ connect.py
@@ -2,24 +2,36 @@ from sqlalchemy import create_engine
 import mysql.connector
 import os
 from dotenv import load_dotenv
+import pandas as pd
+from pathlib import Path
 
-def get_variables():
-    load_dotenv('.env')
-    username = os.environ['USER']
-    passwd = os.environ['PASSWD']
-    database = os.environ['DATABASE']
-    host = os.environ['HOST']
-    return username, passwd, database, host
+def get_variables(by_type):
+    env_path = Path('C:\\Users\\FATAH\\Documents\\REPO\\PROJECT\\env_file\\get_size.env')
+    load_dotenv(dotenv_path=env_path)
+    username = os.environ['USER'].split(",")
+    passwd = os.environ['PASSWD'].split(",")
+    database = os.environ['DATABASE'].split(",")
+    host = os.environ['HOST'].split(",")
+    db_type = os.environ['DB_TYPE'].split(",")
+    db_driver = os.environ['DB_DRIVER'].split(",")
+
+    df = pd.DataFrame(list(zip(username,passwd,database,host,db_type,db_driver)),columns=['username','passwd','database','host','db_type','db_driver'])
+    data = df.query("db_type == @by_type")
+
+    return data['username'].iloc[0],data['passwd'].iloc[0],data['database'].iloc[0],data['host'].iloc[0],data['db_driver'].iloc[0]
 
 
-def sqlalchemy_conn():
-    username, passwd, db, hostname = get_variables()
-    try:
-        engine = create_engine(f'mysql://{username}:{passwd}@{hostname}/{db}')
-        conn = engine.connect()
-    except Exception as error:
-        print('cant connect to db',error)
-    return conn
+def sqlalchemy_conn(db_type):
+    for x in db_type:
+        username, passwd, db, hostname,db_driver = get_variables(x)
+        #print(x)
+        try:
+            engine = create_engine(f'{db_driver}://{username}:{passwd}@{hostname}/{db}')
+            conn = engine.connect()
+        except Exception as error:
+            print('cant connect to db',error)
+        else:
+            return conn
 
 def mysql_conn():
     username, passwd, db, hostname = get_variables()
@@ -32,3 +44,6 @@ def mysql_conn():
 
 def postgres_conn():
     print('a')
+
+
+sqlalchemy_conn(['mysql'])
